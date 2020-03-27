0ee21c1deb5fc1b570a7f0fc35588dfe27dd85d9
Orlova-Olya <58329341+Orlova-Olya@users.noreply.github.com>
2020-03-27 18:24:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    let tabsItem = $('.tabs-item');
+
+    tabsItem.on('click', function (event) {
+        event.preventDefault();
+        $('.tabs-item-active').removeClass('tabs-item-active');
+        $(this).toggleClass('tabs-item-active animated flipInX');
+
+        $('.visible').removeClass('visible');
+        let activeContent = $(this).attr('href');
+        $(activeContent).toggleClass('visible');
+
+    });
+
+});
